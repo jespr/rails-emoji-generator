4f63b6b963ebf07851f4c455be679c6785665ae1
Jesper Christiansen <hi@jespr.com>
2023-10-09 10:57:59 -0700

    require open-uri

--- app/controllers/webhooks/emojis_controller.rb
+++ app/controllers/webhooks/emojis_controller.rb
@@ -3,6 +3,7 @@ module Webhooks
     skip_before_action :verify_authenticity_token
 
     def create
+      require "open-uri"
       emoji = Emoji.find_by(prediction_id: params[:id])
 
       prediction = Replicate.client.retrieve_prediction(emoji.prediction_id)
